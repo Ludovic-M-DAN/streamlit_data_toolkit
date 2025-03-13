0873dcf036fb17c170e0dcc52f32c0b520852fab
Ludovic Marchetti <109848376+Ludovic-M-DAN@users.noreply.github.com>
2025-03-13 15:47:04 +0100

    Suppression des fichiers batch du suivi Git

--- run_tests.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call venv\Scripts\activate
-set PYTHONPATH=%PYTHONPATH%;%CD%\src
-pytest tests/
-pause
--- start_app.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d C:\Users\ludov\streamlit_data_toolkit
-call venv\Scripts\activate
-streamlit run app.py --server.maxUploadSize=5000
